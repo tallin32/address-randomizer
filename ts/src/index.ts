0b311a7d887c8e0919ac2cbb781e938f049cadaa
Chris Meredith <tallin32@gmail.com>
2020-03-02 13:37:53 -0500

    Added build scripts, renamed main.ts to index.ts

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import "reflect-metadata";
+;import express from 'express';
+import { RegisterRoutes } from "./routes";
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const app = express();
-app.get("/foo", (req, res) => {
-    res.json({name: "Emily"})   
+
+RegisterRoutes(app);
+app.use(function(err: any, req: express.Request, res: express.Response, next: any) {
+    res.status(500).json(err);
+})
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get("/version", (req: any, res: any) => {
+    res.json({version: "0.1.1"});
 })
 app.listen(3000);
-
